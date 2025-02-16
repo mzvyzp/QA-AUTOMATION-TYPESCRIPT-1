49c5be1d93923430ab9b738008dac7d5213d26b6
mzviahin <mihanik.zp@gmail.com>
2025-02-16 19:51:42 +0100

    changed arrow functions to TS

--- lesson7/src/arrow-functions.ts
+++ lesson7/src/arrow-functions.ts
@@ -1,11 +1,11 @@
-const calculateArraySum = (numArray) => numArray.reduce((a, numArray) => a + numArray, 0);
-const numArray = [1, 2, 3, 4, 10];
-console.log(calculateArraySum(numArray));
+const calculateArraySum = (numArray: number[]): number => numArray.reduce((a: number, num: number) => a + num, 0);
+const numArray1 = [1, 2, 3, 4, 10];
+console.log(calculateArraySum(numArray1));
 
-const stringArray = ['a', 'b', 'c', 'd', 'e'];
+const stringArray1 = ['a', 'b', 'c', 'd', 'e'];
 const numArray2 = [3, 5, 3, 7, 1];
-const showArrays = (strings, numbers) => {
+const showArrays = (strings: string[], numbers: number[]): void => {
     const concatArray = strings.join(' ') + ' ' + numbers.join(' ');
     console.log(concatArray);
 };
-showArrays(stringArray, numArray2);
+showArrays(stringArray1, numArray2);
