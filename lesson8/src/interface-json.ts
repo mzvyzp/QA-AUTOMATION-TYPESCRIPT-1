36ef1b2e00e13b55af98e1d1091e9b7101d96bf4
mzviahin <mihanik.zp@gmail.com>
2025-02-20 00:22:54 +0100

    task 1-4 corrected

--- lesson8/src/interface-json.ts
+++ lesson8/src/interface-json.ts
@@ -1,32 +1,41 @@
+export interface Geo {
+    lat: string;
+    lng: string;
+}
+
+export interface Address {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: Geo;
+}
+
+export interface Company {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+}
+
 export interface User {
     id: number;
     name: string;
     username: string;
     email: string;
-    address: {
-        street: string;
-        suite: string;
-        city: string;
-        zipcode: string;
-        geo: {
-            lat: string;
-            lng: string;
-        };
-    };
+    address: Address;
     phone: string;
     website: string;
-    company: {
-        name: string;
-        catchPhrase: string;
-        bs: string;
-    };
+    company: Company;
 }
-async function getJson(): Promise<User[]> {
+
+export async function getJson(): Promise<User[]> {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    return response.json();
+    const json = await response.json() as User[];
+    return json;
 }
+
 (async () => {
     const users = await getJson();
-    console.log(users);
-}
-)();
+    console.log('user name:', users[2].name);
+    console.log('company name:', users[2].company.name);
+})();
